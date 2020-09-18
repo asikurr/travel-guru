c8a30367fb8aea623e030c3ddddd99db5841082d
Asikur <asikurrahman7@gmail.com>
2020-09-18 18:48:29 +0600

    travel guru website using react js , google map, firebase auth. google signin, facebook signin, user signup and signin.

---
+++ src/components/Login/firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkg-rQY1hcSz-XkjMhyq7E05p-xrAqfCU",
+    authDomain: "travel-guru-92053.firebaseapp.com",
+    databaseURL: "https://travel-guru-92053.firebaseio.com",
+    projectId: "travel-guru-92053",
+    storageBucket: "travel-guru-92053.appspot.com",
+    messagingSenderId: "419495279800",
+    appId: "1:419495279800:web:0b9b6ac7a1ecad2089084d"
+  };
+
+export default firebaseConfig;
